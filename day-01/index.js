d40fb1fde89f6f0bbac8805937067a69b3994635
Brien Thomas <thomasbrien94@gmail.com>
2022-09-19 16:31:04 -0700

    test written for PRIM03-at, looks for a value at the index number and returns the value

--- day-01/index.js
+++ day-01/index.js
@@ -12,4 +12,23 @@ function titleCase(sentence) {
     .map((word) => word[0].toUpperCase() + word.slice(1)).join(' ');
 
 }
-module.exports = { reverseSentence, titleCase };
+
+function oddishOrEvenish(number) {
+  const a = number
+    .toString();
+  console.log(a);
+  let b = 0;
+  for (let i = 0; i < a.length; i++) {
+    b += parseInt(a[i]);
+  }
+  if (b % 2 === 0) {
+    return 'Evenish';
+  } else {
+    return 'Oddish';
+  }
+}
+
+function at(arr, index) {
+  
+}
+module.exports = { reverseSentence, titleCase, oddishOrEvenish, at };
--- day-01/index.test.js
+++ day-01/index.test.js
@@ -1,4 +1,4 @@
-const { reverseSentence, titleCase } = require('./index');
+const { reverseSentence, titleCase, oddishOrEvenish, at } = require('./index');
 
 describe('primitive code challenges', () => {
   describe('reverseSentence', () => {
@@ -13,5 +13,17 @@ describe('primitive code challenges', () => {
     });
   });
 
-   
+  describe('oddishOrEvenish', () => { 
+    it('should return output for the sum of all numbers', () => {
+      expect(oddishOrEvenish(121)).toBe('Evenish');
+      expect(oddishOrEvenish(41)).toBe('Oddish');
+    });
+  });
+
+  describe('at', () => {
+    it('should return the value of the array at the specified index', () => {
+      expect(at['a', 'b', 'c', 'd', 'e'], 1).toBe('b');
+      expect(at['a', 'b', 'c', 'd', 'e'], - 2).toBe('d');
+    });
+  });
 });
