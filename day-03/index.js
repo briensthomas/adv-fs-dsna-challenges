2bf51a5915dcbbe1c57ef5c04eab180f6034b508
Brien Thomas <thomasbrien94@gmail.com>
2022-09-22 17:14:07 -0700

    Tests written for PersonTreeNode findPerson functions, expects the left and right values of the root (Jones) to match the diagram supplied, and expects the findPerson function to return Nelson

--- day-03/index.js
+++ day-03/index.js
@@ -28,4 +28,39 @@ class BinaryTreeNode {
   }
 }
 
-module.exports = { BinaryTreeNode };
+class PersonTreeNode {
+  constructor(person) {
+    this.value = person.name;
+    this.person = person;
+    this.left = null;
+    this.right = null;
+  }
+  
+  add(node) {
+
+    if (node.person.name === this.value) {
+      return null;
+    } if (node.person.name < this.value) {
+      //   console.log('node.left', node.left);
+      if (this.left == null) {
+        this.left = node;
+      } else {
+        this.left.add(node);
+      }
+    } else {
+      if (this.right == null) {
+        this.right = node;
+
+      } else { 
+        this.right.add(node);
+      }
+    }
+  }
+  
+  findPerson(name) {
+    // Implement me!
+    
+  }
+}
+
+module.exports = { BinaryTreeNode, PersonTreeNode };
--- day-03/index.test.js
+++ day-03/index.test.js
@@ -1,4 +1,4 @@
-const { BinaryTreeNode } = require('../day-03/index');
+const { BinaryTreeNode, PersonTreeNode } = require('../day-03/index');
 
 
 describe('node based binary tree', () => {
@@ -12,13 +12,28 @@ describe('node based binary tree', () => {
     B.add(D);
     B.add(C);
 
-    console.log('Test Line 14: ', B);
-    console.log('A', A);
-    console.log('D from Test', D);
-    console.log('C', C);
-    console.log('B from Test', B);
     expect(B.left.value).toEqual('A');
     expect(B.right.value).toEqual('D');
     expect(B.right.left.value).toEqual('C');
+  });
+});
+
+describe('Person Binary Tree', () => {
+  it('should return the person object matching the name', () => {
+    const Jones = new PersonTreeNode('Jones');
+    const Brook = new PersonTreeNode('Brook');
+    const Smith = new PersonTreeNode('Smith');
+    const Nelson = new PersonTreeNode('Nelson');
+
+    Jones.add(Brook);
+    Jones.add(Smith);
+    Jones.add(Nelson);
+
+    const person1 = PersonTreeNode.findPerson('Nelson');
+
+    expect(Jones.left.name).toEqual('Brook');
+    expect(Jones.right.name).toEqual('Smith');
+    expect(Jones.right.left.name).toEqual('Nelson');
+    expect(person1).toEqual('Nelson');
   });
 });
